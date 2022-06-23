7edd8164a8d283cf75365bfdb58986bc36564735
Sean Wong <seanwong24@live.com>
2022-06-23 11:15:25 -0700

    update metadata style

--- src/components/vis-main-sidebar-metadata/vis-main-sidebar-metadata.tsx
+++ src/components/vis-main-sidebar-metadata/vis-main-sidebar-metadata.tsx
@@ -16,12 +16,12 @@ export class VisMainSidebarMetadata implements ComponentInterface {
       <Host>
         <vis-main-collapse>
           <h3 slot="header">Metadata</h3>
-          <div style={{ height: '10rem', width: '100%' }}>
+          <div style={{ height: 'auto', width: '100%' }}>
             {Object.entries(this.data.layerMetadataMap?.get(this.data?.selection?.layer)?.[this.data?.selection?.id]?.data || {}).map(([key, value]) => (
-              <span>
-                {key}: {value}
-                <br />
-              </span>
+              <vis-main-collapse style={{ margin: '.5rem' }}>
+                <b slot="header">{key}</b>
+                <div innerHTML={value.toString()} style={{ overflowWrap: 'break-word' }}></div>
+              </vis-main-collapse>
             ))}
           </div>
         </vis-main-collapse>
